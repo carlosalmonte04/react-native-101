26bb4a173a7fdbbd11dc52a185a2a75ad1f74467
carlosalmonte04 <vacarlos_alm@hotmail.com>
2019-02-20 11:43:52 -0500

    [CHORE] Add constants and react native config

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import moment from "moment";
+import md5 from "js-md5";
 import { RootNavigator } from "@navigation";
 import { store } from "@redux";
 import { Provider } from "react-redux";
+import { MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY } from "@constants";
 
 export default class App extends Component {
+  async componentDidMount() {
+    const ts = moment().valueOf();
+    const hash = md5(`${ts}${MARVEL_PRIVATE_KEY}${MARVEL_PUBLIC_KEY}`);
+    const charsRes = await fetch(
+      `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${MARVEL_PUBLIC_KEY}&hash=${hash}`,
+    );
+
+    const chars = await charsRes.json();
+
+    console.log("CHARS", chars);
+  }
+
   render() {
     return (
       <Provider store={store}>
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+import Config from "react-native-config";
+
+const { MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY } = Config;
+
+export { MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY };
